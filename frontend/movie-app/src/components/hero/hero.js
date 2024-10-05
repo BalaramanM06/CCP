21f7f74fcc61b9fdffb090fd0606f675ba5ce17c
D-206 <162539953+D-206@users.noreply.github.com>
2024-10-05 18:18:46 +0530

    r

---
+++ frontend/movie-app/src/components/hero/hero.js
@@ -0,0 +1,13 @@
+import './Hero.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+
+
+
+const hero = () => {
+  return (
+    <div>hero</div>
+  )
+}
+
+export default hero
